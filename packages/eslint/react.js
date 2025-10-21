e3416500c7c58f63e20968450d14162163805874
Sain-Biswas <sain.biswas614@gmail.com>
2025-10-22 04:37:37 +0530

    chore: update bun.lock

--- packages/eslint/react.js
+++ packages/eslint/react.js
@@ -18,31 +18,31 @@ export const reactESLintConfig = defineConfig([
     files: ["**/*.{ts,tsx,js,mjs}"],
     plugins: {
       "react-hooks": pluginReactHooks,
-      react: pluginReact,
+      react: pluginReact
     },
     rules: {
       ...pluginReactHooks.configs["recommended-latest"].rules,
       ...pluginReact.configs.recommended.rules,
       ...pluginReact.configs["jsx-runtime"].rules,
-      "react/react-in-jsx-scope": "off",
+      "react/react-in-jsx-scope": "off"
     },
     settings: {
       react: {
-        version: "detect",
-      },
+        version: "detect"
+      }
     },
     languageOptions: {
       ecmaVersion: 2022,
       globals: {
-        ...globals.browser,
+        ...globals.browser
       },
       parserOptions: {
         ecmaFeatures: {
-          jsx: true,
-        },
-      },
-    },
+          jsx: true
+        }
+      }
+    }
   },
   pluginTanstackQuery.configs["flat/recommended"],
-  pluginTanstackRouter.configs["flat/recommended"],
+  pluginTanstackRouter.configs["flat/recommended"]
 ]);
